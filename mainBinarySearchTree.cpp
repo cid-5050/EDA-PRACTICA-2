c43e34a6c66998984ccb3efd11a7e9b8db1b6823
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2022-03-27 20:01:51 +0200

    Add files via upload

--- mainBinarySearchTree.cpp
+++ mainBinarySearchTree.cpp
@@ -1,19 +1,10 @@
 #include "BinarySearchTree.h"
 #include <iostream>
 
+using namespace std;
 
 int main(){
     BinarySearchTree<int> s;
     s.insert(2);
-    s.insert(4);
-    s.insert(1);
-    s.insert(9);
-    s.insert(7);
-
-    //s.removeMin();
-
-    //std::cout << s.find(9).get() << std::endl;
-    //std::cout << s.find(1).get() << std::endl;
-
     return 0;
 }
